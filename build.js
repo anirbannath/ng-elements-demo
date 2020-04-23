1b749da85b0c3268e0bdb7b676986a399ee73777
Anirban Nath <ani.djrocker@gmail.com>
2020-04-23 19:03:09 +0530

    Styling changes

--- build.js
+++ build.js
@@ -15,7 +15,7 @@ const concat = require('concat');
 
   await fs.ensureDir('dist/public');
   await concat(html, 'dist/public/index.html');
-  await concat(js, 'dist/public/ng-elements-poc.js');
+  await concat(js, 'dist/public/ng-elements-demo.js');
 
 })();
 
